5851b443e980eb55d4e33a1a3938fddf006b3c19
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-12-10 16:51:03 -0500

    laid out foundation for the Car Router for CRUD operations

---
+++ cars/router.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const db = require("../dbConfig.js");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {});
+
+router.get("/:id", (req, res) => {});
+
+router.post("/", (req, res) => {});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const CarRouter = require("./cars/router.js");
+
 const server = express();
 
 server.use(express.json());
@@ -7,5 +9,7 @@ server.use(express.json());
 server.get("/", (req, res) => {
   res.send("<h2>Tuesday, December 10th Project.</h2>");
 });
+
+server.use("/api/cars", CarRouter);
 
 module.exports = server;
